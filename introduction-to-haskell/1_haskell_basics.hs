de4a8ffcf9d148210eda0c79da8354df5b76adae
DjebbZ <khalid.jebbari@gmail.com>
2013-09-21 20:08:47 +0200

    Finished the Arithmetic part

--- introduction-to-haskell/1_haskell_basics.hs
+++ introduction-to-haskell/1_haskell_basics.hs
@@ -4,5 +4,12 @@
 
 i = 30 :: Int
 j = 10 :: Integer
-main = print (i + (fromInteger j))
+--main = print (i + (fromInteger j))
 -- Error because `+` works on values of same numeric type
+
+x = 20 :: Int
+--main = print (i / i)
+-- Error because `/` performs floating-point division only
+main = print (i `div` i)
+
+-- Conclusion : No implicit conversion of numeric types
