c70c2f5b5d11518c624ee724e0285f7d6b6208f7
DjebbZ <khalid.jebbari@gmail.com>
2013-09-21 20:17:43 +0200

    Boolean Logic

--- introduction-to-haskell/1_haskell_basics.hs
+++ introduction-to-haskell/1_haskell_basics.hs
@@ -10,6 +10,21 @@ j = 10 :: Integer
 x = 20 :: Int
 --main = print (i / i)
 -- Error because `/` performs floating-point division only
-main = print (i `div` i)
+--main = print (i `div` i)
 
 -- Conclusion : No implicit conversion of numeric types
+
+
+
+-- ----------------------
+-- Boolean Logic
+-- ----------------------
+
+ex1 = True && False -- False of course
+ex2 = not (False || True) -- True
+ex3 = ('a' == 'a')
+ex4 = 16 /= 3 -- works with and without parentheses
+ex5 = "Haskell" > "C++" -- True
+
+main = print ex5
+
