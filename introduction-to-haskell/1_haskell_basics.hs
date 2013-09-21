08b36032c73ec366ab4ce83816aeffccc5a4b8b6
DjebbZ <khalid.jebbari@gmail.com>
2013-09-21 20:33:28 +0200

    Finished "Defining Basic Functions"

--- introduction-to-haskell/1_haskell_basics.hs
+++ introduction-to-haskell/1_haskell_basics.hs
@@ -26,5 +26,37 @@ ex3 = ('a' == 'a')
 ex4 = 16 /= 3 -- works with and without parentheses
 ex5 = "Haskell" > "C++" -- True
 
-main = print ex5
+--main = print ex5
 
+
+
+-- ----------------------
+-- Defining Basic Functions
+-- ----------------------
+
+-- function's type
+sumtorial :: Integer -> Integer
+-- pattern matching
+sumtorial 0 = 0
+sumtorial n = n + sumtorial (n - 1)
+
+--main = print (sumtorial 10)
+
+hailstone :: Integer -> Integer
+-- using guards (i.e. pattern matching with Boolean expresions)
+hailstone n
+    | n `mod` 2 == 0    = n `div` 2
+    | otherwise         = 3 * n + 1
+
+--main = print (hailstone 3)
+
+isEven :: Integer -> Bool
+isEven n
+    | n `mod` 2 == 0    = True
+    | otherwise         = False
+
+-- too complicated. Much simpler to write
+isEven' :: Integer -> Bool
+isEven' n = n `mod` 2 == 0
+
+main = print [isEven 2, isEven' 5]
