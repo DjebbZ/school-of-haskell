32b4707cea5091ba66e522839f71eede6c1602ad
DjebbZ <khalid.jebbari@gmail.com>
2013-09-22 12:36:21 +0200

    Case Expressions and Recursive Data Types

--- introduction-to-haskell/2_algebraic_data_types.hs
+++ introduction-to-haskell/2_algebraic_data_types.hs
@@ -109,5 +109,58 @@ watsYourName p@(Person n _ _ ) = "The name field of (" ++ show p ++ ") is " ++ n
 
 --main = putStrLn (watsYourName brent) -- The name field of (Person "Brent" 30 SealingWax) is Brent
 -- putStrLn instead of print because it's already a string
-main = print (watsYourName brent) -- "The name field of (Person \"Brent\" 30 SealingWax) is Brent"
+--main = print (watsYourName brent) -- "The name field of (Person \"Brent\" 30 SealingWax) is Brent"
 -- wraps result in double quotes, so it escape the quotes already present
+
+
+
+-- ----------------------
+-- Case Expressions
+-- ----------------------
+
+-- case expression is the fundamental construct for doing pattern matching
+value = case "Hello" of
+    []      -> 0        -- not matched, try next
+    ('H':s) -> length s -- matched, returns 4
+    _       -> 7        -- would match but is never reached
+
+-- pattern matching in function is just syntactic sugar for case expressions
+failureToNumber' :: FailableDouble -> Double
+failureToNumber' x = case x of
+    Failure -> 0
+    OK d    -> d
+
+--main = print (failureToNumber' failed, failureToNumber' ok) -- (0.0,3.4)
+
+
+
+-- ----------------------
+-- Recursive Data Types
+-- ----------------------
+
+-- Built-in [] is recurisve, it's constructed from an element and a list with the rest :
+-- (pseudo-code) data List = x:List
+-- Cons would be ":", Empty would be [], IntList would be [Int]
+data IntList = Empty | Cons Int IntList
+
+intListProd :: IntList -> Int
+intListProd Empty = 1
+intListProd (Cons x xs) = x * intListProd xs
+
+--main = print (intListProd (Cons 3 (Cons 2 (Cons 4 Empty)))) -- 24
+
+-- equivalent with built-in types
+intListProd' :: [Int] -> Int
+intListProd' [] = 1
+intListProd' (x:xs) = x * intListProd' xs
+
+--main = print (intListProd' [3,2,4]) -- 24
+
+data Tree = Leaf Char
+            | Node Tree Int Tree
+    deriving Show
+
+tree :: Tree
+tree = Node (Leaf 'x') 1 (Node (Leaf 'y') 2 (Leaf 'z'))
+
+main = print tree -- Node (Leaf 'x') 1 (Node (Leaf 'y') 2 (Leaf 'z'))
